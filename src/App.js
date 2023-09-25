14a19d6bb2571200263325f7ce71be3aa1e1bbc3
carolinelilyyu <carolinelilyyu@gmail.com>
2023-09-25 10:57:19 -0400

    links

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Web Development Fall 2022
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+        <p>Lab 1</p>
+        <a href="../labs/a1/index.html">Lab a1</a>
+        <p>Assignment 1</p>
+        <a href="../kanbas/home.html">Assignment 1</a>
+      
     </div>
   );
 }
