8bf872c07561fa7a913f0353028b6477fde5e791
carolinelilyyu <carolinelilyyu@gmail.com>
2023-09-26 20:58:17 -0400

    kanbas assignment 2 so far

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
   return (
     <div className="App">
        
-        <p>Lab 1</p>
-        <a href="../labs/a1/index.html">Lab a1</a>
-        <p>Assignment 1</p>
-        <a href="../kanbas/home.html">Assignment 1</a>
+        <p>Labs</p>
+        <a href="/labs/a1/index.html">Lab a1</a>
+        <br/>
+        <a href="/labs/a2/index.html">Lab a2</a>
+        <br/>
+
+        <p>Assignments</p>
+        <a href="/kanbas/index.html">Assignment 1</a>
       
     </div>
   );
