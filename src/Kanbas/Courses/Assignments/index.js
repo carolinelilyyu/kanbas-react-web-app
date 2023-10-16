a5e3116285bb01af83b208eacd5bee8c9fb6a818
carolinelilyyu <carolinelilyyu@gmail.com>
2023-10-15 23:22:07 -0400

    more css updates

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import db from "../../Database";
-
+import "./index.css"
 
 function Assignments() {
   const { courseId } = useParams();
@@ -10,17 +10,58 @@ function Assignments() {
     (assignment) => assignment.course === courseId);
   return (
     <div>
+
+
       <h2>Assignments for course {courseId}</h2>
       <div className="list-group">
+
+        {/* <!-- Third column --> */}
+        <div class="wd-flex-grow-1">
+              {/* <!-- buttons on top --> */}
+              <div class="d-flex justify-content-between">
+                  <input type="text" id="searchassignments"
+                  placeholder="Search for Assignments" class="form-control w-25"/>
+      
+                  <div class="d-flex">
+                      <button type="button" class="btn btn-light">Group</button>
+                      <button type="button" class="btn btn-danger">Assignment</button>
+                      <button type="button" class="btn btn-light"><i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i></button>
+                  </div>
+              </div>
+
+              
+              {/* <!-- Assignments --> */}
+              <ul class="list-group rounded-0">
+                  <li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">ASSIGNMENTS
+                      <div class="d-flex justify-content-between">
+                          <span class="ps-2 badge badge-pill badge-dark rounded">40% of Total</span>
+                          <i class="fa fa-check-circle" aria-hidden="true"></i>
+                          <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+                      </div>
+                  </li>
+              </ul>
+        </div>
         {courseAssignments.map((assignment) => (
           <Link
-            key={assignment._id}
-            to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
-            className="list-group-item">
-            {assignment.title}
-          </Link>
-        ))}
+          key={assignment._id}
+          to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+          className="list-group-item">
+          {/* Assignments */}
+          <a href="/kanbas/assignments/edit.html" class="list-group-item list-group-item-action ps-5 d-flex justify-content-between flex-column align-items-start">
+              <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">{assignment.title}</h5>
+              <small class="text-muted"><i class="fa fa-check-circle" aria-hidden="true"></i>
+                  <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i></small>
+              </div>
+          <p class="mb-1">{assignment.course}</p>
+          <small class="text-muted">Due {assignment.duedate}</small>
+          </a>
+                  
+
+        </Link>
+      ))}
       </div>
+
     </div>
   );
 }
--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -6,18 +6,206 @@ function ModuleList() {
   const { courseId } = useParams();
   const modules = db.modules;
   return (
+    <div>
+
     <ul className="list-group">
+      <div class="d-flex float-end">
+          <button type="button" class="btn btn-light">Collapse All</button>
+          <button type="button" class="btn btn-light">View Progress</button>
+          <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="fa fa-check-circle" aria-hidden="true"></i>
+                  Publish All
+              </button>
+              <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+          </div>
+          <button type="button" class="btn btn-danger">Module</button>
+          <button type="button" class="btn btn-light"><i class="fa fa-ellipsis-v" aria-hidden="true"></i>
+          </button>
+      </div>
+      <ul class="list-group mb-5">
+                <li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">Resources
+                    <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+                        <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+                    </span>
+                </li>
+            </ul>
+      <ul class="list-group mb-5">
+          <li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">Required Textbooks
+              <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+                  <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+              </span>
+          </li>
+      </ul>
       {
        modules
          .filter((module) => module.course === courseId)
          .map((module, index) => (
-           <li key={index} className="list-group-item">
-             <h3>{module.name}</h3>
-             <p>{module.description}</p>
-           </li>
+          <div>
+          <ul class="list-group mb-5">
+                <li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">Week {module.week} - {module.name}
+                    <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+                        <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+                    </span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">LEARNING OBJECTIVES
+                    <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+                        <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+                    </span>
+                </li>
+
+                <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">{module.description}
+                        
+                        <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+                            <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+                        </span>
+                    </li>
+            </ul>
+            
+          </div>
+           
       ))
       }
     </ul>
+    </div>
+
+            // <ul>
+                
+
+
+            // </div>
+            //     <br/>
+            //     <hr>
+            //     <!-- week 0 -->
+            //     <ul class="list-group mb-5">
+            //         <li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">Week 0 - INTRO
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <!-- Learning objectives -->
+            //         <li class="list-group-item d-flex justify-content-between align-items-center">LEARNING OBJECTIVES
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Introduction to the course
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Learn what is Web Development
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Creating a development environment
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Creating a Web Application
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Getting started with the 1st assignment
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         </li>
+            //         <!-- Reading -->
+            //         <li class="list-group-item d-flex justify-content-between align-items-center">READING
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Full Stack Developer - Chapter 1 - Introduction
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Full Stack Developer - Chapter 2 - Creating User Interfaces with HTML
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+                    
+            //         <!-- Slides -->
+            //         <li class="list-group-item d-flex justify-content-between align-items-center">SLIDES
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center"><a href="www.youtube.com" class="link-danger">Introduction to Web Development Links to an external site.</a>
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center"><a href="www.youtube.com" class="link-danger">Creating an HTTP server with Node.js Links to an external site.</a>
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center"><a href="www.youtube.com"  class="link-danger">Creating a React Application Links to an external site.</a>
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //       </ul>
+            //       <!-- week 1 -->
+            //       <ul class="list-group mb-5">
+            //         <li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">Week 1 - HTML
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <!-- Learning objectives -->
+            //         <li class="list-group-item d-flex justify-content-between align-items-center">LEARNING OBJECTIVES
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Learn how to create user interfaces with HTML
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Keep working on assignment 1
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Deploy the assignment to Netlify
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <!-- Reading -->
+            //         <li class="list-group-item d-flex justify-content-between align-items-center">READING
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Full Stack Developer - Chapter 1 - Introduction
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+            //         <li class="list-group-item ps-5 d-flex justify-content-between align-items-center">Full Stack Developer - Chapter 2 - Creating User Interfaces with HTML
+            //             <span><i class="fa fa-check-circle" aria-hidden="true"></i>
+            //                 <i class="fa fa-ellipsis-v ps-2" aria-hidden="true"></i>
+            //             </span>
+            //         </li>
+                
+                
   );
 }
 export default ModuleList;
