d5e522f9fd5343df8a9e532e9481eb0029ef4cae
carolinelilyyu <carolinelilyyu@gmail.com>
2023-11-02 15:33:36 -0400

    a4

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
 import {AiOutlineCheckCircle} from "react-icons/ai";
-import {FaEllipsisVertical} from "react-icons/fa6"
+import {FaEllipsisVertical} from "react-icons/fa6";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./modulesReducer";
+
 function ModuleList() {
   const { courseId } = useParams();
-  const modules = db.modules;
+  const modules = useSelector((state) => state.modulesReducer.modules);
+  const module = useSelector((state) => state.modulesReducer.module);
+  const dispatch = useDispatch();
+
   return (
     <div>
         <div class="wd-flex-row-container">
             <div class="wd-flex-grow-1 d-none d-sm-block">
                     <hr />
+                    
                     <div class="d-flex float-end">
                         <button type="button" class="btn btn-light">Collapse All</button>
                         <button type="button" class="btn btn-light">View Progress</button>
@@ -32,6 +44,28 @@ function ModuleList() {
                     </div>
                     <br/>
                     <hr />
+                    <li className="list-group-item">
+                                <input value={module.name}
+                                onChange={(e)=> dispatch(setModule({ ...module, name: e.target.value }))
+                                }
+                                />
+                                <br/>
+                                <textarea value={module.description}
+                                onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))                            
+                                }
+                                />
+                                <br/>
+                                <button type="button" class="btn btn-danger" 
+                                          onClick={() => dispatch(addModule({ ...module, course: courseId }))}>
+                                          Add</button>
+                                <button 
+                                type="button" class="btn btn-light"
+                                onClick={() => dispatch(updateModule(module))}>
+                                Update
+                                </button>
+
+                            </li>
+                            <br/>
 
                     <ul class="list-group mb-5">
                         <li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center"> Readings
@@ -47,6 +81,7 @@ function ModuleList() {
                             </span>
                         </li>
                     </ul>
+                   
                     {
                     modules
                         .filter((module) => module.course === courseId)
@@ -54,6 +89,18 @@ function ModuleList() {
                         <div>
                             <ul class="list-group mb-5">
                                 <li class="list-group-item list-group-item-secondary d-flex justify-content-between align-items-center">Week {module.week} - {module.name}
+                                    <button
+                                    type="button" class="btn btn-light"
+                                    onClick={() => dispatch(setModule(module))}>
+                                    Edit
+                                    </button>
+
+                                    <button
+                                     type="button" class="btn btn-danger"
+                                     onClick={() => dispatch(deleteModule(module._id))}>
+                                     Delete
+                                    </button>
+
                                     <span><AiOutlineCheckCircle className="fa-check-circle"/>
                                         <FaEllipsisVertical />
                                     </span>
--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,13 +1,36 @@
 import { Link } from "react-router-dom";
+import { React, useState } from "react";
+
 import db from "../Database";
 import "./index.css";
 import "../../styles.css";
 
-function Dashboard(){
-    const courses = db.courses;
+function Dashboard({ courses, course, setCourse, addNewCourse,
+  deleteCourse, updateCourse }){
     return (
         <div className="wd-dashboard">
+          <h5>Course</h5>
+          <input value={course.name} className="form-control" 
+                       onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+
+          <input value={course.number} className="form-control"
+                       onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+
+          <input value={course.startDate} className="form-control" type="date" 
+                       onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+
+          <input value={course.endDate} className="form-control" type="date" 
+                       onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+
           <h1>Dashboard</h1>
+          <button onClick={addNewCourse} >
+            Add
+          </button>
+          
+          <button onClick={updateCourse} >
+            Update
+          </button>
+
           <hr />
           <div className="container">
             <h2>Published Course ({courses.length})</h2>
@@ -21,6 +44,21 @@ function Dashboard(){
                         <h5 class="card-title">{course._id}</h5>
                         <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="list-group-item">
                           <b>{course.name} {course.number}</b>
+                          <br/>
+                          <button
+                          onClick={(event) => {
+                            event.preventDefault();
+                            setCourse(course);
+                          }}>
+                          Edit
+                        </button>
+                          <button
+                          onClick={(event) => {
+                            event.preventDefault();
+                            deleteCourse(course._id);
+                          }}>
+                          Delete
+                        </button>  
                         </Link>
                         <p class="text-muted">{course.startDate} TO {course.endDate}</p>
 
